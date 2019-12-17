6c2d443ccacabb53caae7cd68d941ee209abc15a
Oleksii Zharikov <mefis2.0@gmail.com>
2019-12-17 15:02:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// let a = 'global';
+// function outer (){
+//     let b = 'outer';
+//     function inner (){
+//         let c = 'inner';
+//         console.log(c);
+//         console.log(b);
+//         console.log(a);
+//     }
+//     console.log(a);
+//     console.log(b);
+//     inner();
+// }
+// outer();
+// console.log(a);
+
+
+// let users = {
+//     Alan: {
+//       age: 27,
+//       online: true
+//     },
+//     Jeff: {
+//       age: 32,
+//       online: true
+//     },
+//     Sarah: {
+//       age: 48,
+//       online: true
+//     },
+//     Ryan: {
+//       age: 19,
+//       online: false
+//     }
+//   };
+  
+//   function countOnline(users) {
+//     // change code below this line
+
+//       let result = 0;
+//         for (let user in users){
+//           if (users[user].online === true){
+//             result++;
+//           }
+//         }
+//         return result;
+//     // change code above this line
+//   }
+//   console.log(countOnline(users));
+
+// function mult (a,b){
+//     let c = 0;
+//     for (let i = 0; i < b; i++){
+//         c = c + a;
+//     }
+//     return c;
+// }
+// console.log(mult(2,15));
+
